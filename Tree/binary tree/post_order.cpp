161c8ede7c6262eab5e9887f5be0a202cd0761c9
MD. Rafiul Islam <rafiulislam316@gmail.com>
2026-01-30 10:34:31 +0600

    Add post-order traversal implementation
    Implement post-order traversal for a binary tree.

---
+++ Tree/binary tree/post_order.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int val;
+        Node * left;
+        Node * right;
+    Node (int val) {
+        this -> val = val;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+    
+    
+};
+
+void post_order (Node *root) {
+    
+    if(root == NULL) return;
+    post_order(root -> left) ;
+    cout << root->val << " ";
+    post_order(root -> right) ;
+}
+
+
+int main() {
+	Node  * root = new Node(10);
+	Node * a = new Node(20);
+	Node * b = new Node(30);
+	Node * c = new Node(40);
+	Node * d = new Node(50);
+	Node * e = new Node(60);
+	
+	
+	root -> left = a;
+	root -> right = b;
+	a -> left = c;
+	a -> right = d;
+	b -> left = e;
+	
+	post_order(root);
+	return 0;
+	
+
+}
